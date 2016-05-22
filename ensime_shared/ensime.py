c0d56d95fa22f9d57523fde74d74071dd6d56350
Yash Yadavalli <yyadavalli@users.noreply.github.com>
2016-05-22 09:37:07 -0400

    Consitently place the log file in .ensime_cache (#264)
    * Consitently place the log file in .ensime_cache
    
    * Handle failure when creating directory

--- ensime_shared/ensime.py
+++ ensime_shared/ensime.py
@@ -105,8 +105,12 @@ class EnsimeClient(DebuggerClient, object):
             self.config_path = osp.abspath(config_path)
             config_dirname = osp.dirname(self.config_path)
             self.ensime_cache = osp.join(config_dirname, ".ensime_cache")
-            self.log_dir = self.ensime_cache \
-                if osp.isdir(self.ensime_cache) else "/tmp/"
+            self.log_dir = self.ensime_cache
+            if not osp.isdir(self.ensime_cache):
+                try:
+                    os.mkdir(self.ensime_cache)
+                except OSError as exception:
+                    self.log_dir = "/tmp/"
             self.log_file = os.path.join(self.log_dir, "ensime-vim.log")
             with open(self.log_file, "w") as f:
                 now = datetime.datetime.now()
